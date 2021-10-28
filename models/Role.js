572def86a96a03d1c1c483a1baea3d0eedbb5355
Vlad <juzopigeon@gmail.com>
2021-10-28 12:36:10 +0600

    Create Role.js

---
+++ models/Role.js
@@ -0,0 +1,13 @@
+const {
+  Schema,
+  model
+} = require('mongoose'
+
+)
+
+
+const User = new Schema({
+        value:  {type: String, unique: true, default: 'USER' }
+})
+
+module.exports = model('User', User)
